9b0f19d7b48a2931e021055b632741d192a4a2fb
Rusik111SergeySILLYANIMAL <150540013+Rusik111SergeySILLYANIMAL@users.noreply.github.com>
2023-11-18 19:45:40 +0300

    Add files via upload

--- NewNPCScript.cs
+++ NewNPCScript.cs
@@ -8,21 +8,21 @@ public class NewNPCScript : MonoBehaviour
 {
     public int health = 5; //здоровье
     public float speed = 3.5f; //скорость
-    public int level = 2;
-    public GameObject obj;
+    public int level = 2; 
+    public GameObject obj; //обращение к объекту
 
     // Start is called before the first frame update
     void Start()
     {
-        health = level;
-        print("Healt count:" + health);
+        health = level; //обновление здоровья
+        print("Healt count:" + health); //вывод здоровья
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = transform.position;
-        newPosition.x += Time.deltaTime * speed;
-        obj.transform.position = new Vector3(newPosition.x, 0, 0);
+        Vector3 newPosition = transform.position; //получение следующей позиции
+        newPosition.x += Time.deltaTime * speed; //вычисление позиции
+        obj.transform.position = new Vector3(newPosition.x, 0, 0); //обновление позиции NPC
     }
 }
